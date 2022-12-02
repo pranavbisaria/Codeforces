32834b75e496c1088b86f2b845961f15b289e0ec
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-12-02 14:35:09 +0530

    A. Watermelon

---
+++ 800/A. Watermelon.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int W;
+    cin>>W;
+    if(W%2==0 && W>2){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+}
