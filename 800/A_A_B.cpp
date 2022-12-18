e8494f61918127b13a305e425a6bf1bafe39ccdc
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-12-18 20:38:54 +0530

    A. A+B?

---
+++ 800/A_A_B.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int a, b;
+        scanf("%d+%d", &a, &b);
+        cout<<(a+b)<<"\n";
+    }
+}
